d60b7ba5f7c587d4734c707f4d0a802f90434fa9
Michael Brown <michael.brown@Viewpoint.com>
2019-06-04 09:55:58 -0700

    exclude acceptance tests !deploy

--- Tests/MessageCompositionObjects.Tests/New-SlackTextObject.Tests.ps1
+++ Tests/MessageCompositionObjects.Tests/New-SlackTextObject.Tests.ps1
@@ -5,90 +5,86 @@ Get-Module PSlickPSlack | Remove-Module -Force
 Import-Module .\PSlickPSlack\PSlickPSlack.psm1
 $functionName = $MyInvocation.MyCommand -replace ".Tests.ps1", ""
 
-Describe "$functionName plain_text Test Object Tests" {
-    
-    $text = "Hello there."
-    $plain_textObject = New-SlackTextObject -type plain_text -text $text
-    $properties = ("type", "text", "emoji")
-    $propertyCount = $properties.Count
+$text = "Hello there."
+$plain_textObject = New-SlackTextObject -type plain_text -text $text
+$properties = ("type", "text", "emoji")
+$propertyCount = $properties.Count
 
-    Describe "Slack plain_text Object unit tests" -Tags "Unit" {
-        It "An object is created" {
-            [bool](New-SlackTextObject -type "plain_text" -text $text) | Should -Be $true
-        }
-        It "has a type of plain_text" {
-            $plain_textObject.type | Should -BeExactly 'plain_text'
-        }
-        It "has text `"$text`"" {
-            $plain_textObject.text | Should -Be $text
-        }
-        It "has property count $propertyCount" {
-            $plain_textObject.PSObject.Properties.Name | Should -HaveCount $properties.Count
-        }
-        foreach ($property in $properties) {
-            It "has a $property property" {
-                [bool]($plain_textObject.PSObject.Properties.Name -match $property) | Should Be $true
-            }
-        }
-        It "is valid JSON" {
-            $plain_textObject | ConvertTo-Json -Depth 100
+Describe "$functionName plain_text Test Object Unit Tests" -Tags "Unit" {
+    It "An object is created" {
+        [bool](New-SlackTextObject -type "plain_text" -text $text) | Should -Be $true
+    }
+    It "has a type of plain_text" {
+        $plain_textObject.type | Should -BeExactly 'plain_text'
+    }
+    It "has text `"$text`"" {
+        $plain_textObject.text | Should -Be $text
+    }
+    It "has property count $propertyCount" {
+        $plain_textObject.PSObject.Properties.Name | Should -HaveCount $properties.Count
+    }
+    foreach ($property in $properties) {
+        It "has a $property property" {
+            [bool]($plain_textObject.PSObject.Properties.Name -match $property) | Should Be $true
         }
     }
-    Describe "Slack plain_text Object Acceptance tests" -Tags "Acceptance" {
-        $slackContent = Get-Content .\slacktoken.json | ConvertFrom-Json
-        $Body = $plain_textObject | ConvertTo-Json -Depth 100
-        It "returns http 200 response" {
-            $params = @{
-                Method      = "Post"
-                Uri         = $slackContent.slackwebhook
-                Headers     = @{Authorization = ("Bearer " + $slackContent.slacktoken) }
-                ContentType = "application/json"
-                Body        = $Body
-            }
-            Invoke-RestMethod @params | Should Be "ok"
-        }
+    It "is valid JSON" {
+        $plain_textObject | ConvertTo-Json -Depth 100
     }
 }
-Describe "$functionName mrdkwn Test Object Tests" {
-    $text = "*Hello* _there_."
-    $mrkdwnObject = New-SlackTextObject -type mrkdwn -text $text
-    $properties = ("type", "text", "verbatim")
-    $propertyCount = $properties.Count
-    
-    Describe "Slack mrkdwn Object unit tests" -Tags "Unit" {
-        It "An object is created" {
-            [bool](New-SlackTextObject -type "plain_text" -text $text) | Should -Be $true
+Describe "Slack plain_text Object Acceptance tests" -Tags "Acceptance" {
+    $slackContent = Get-Content .\slacktoken.json | ConvertFrom-Json
+    $Body = $plain_textObject | ConvertTo-Json -Depth 100
+    It "returns http 200 response" {
+        $params = @{
+            Method      = "Post"
+            Uri         = $slackContent.slackwebhook
+            Headers     = @{Authorization = ("Bearer " + $slackContent.slacktoken) }
+            ContentType = "application/json"
+            Body        = $Body
         }
-        It "has a type of plain_text" {
-            $mrkdwnObject.type | Should -BeExactly 'mrkdwn'
-        }
-        It "has text `"$text`"" {
-            $mrkdwnObject.text | Should -Be $text
-        }
-        It "has property count $propertyCount" {
-            $mrkdwnObject.PSObject.Properties.Name | Should -HaveCount $properties.Count
-        }
-        foreach ($property in $properties) {
-            It "has a $property property" {
-                [bool]($mrkdwnObject.PSObject.Properties.Name -match $property) | Should Be $true
-            }
-        }
-        It "is valid JSON" {
-            $mrkdwnObject | ConvertTo-Json -Depth 100
+        Invoke-RestMethod @params | Should Be "ok"
+    }
+}
+
+$text = "*Hello* _there_."
+$mrkdwnObject = New-SlackTextObject -type mrkdwn -text $text
+$properties = ("type", "text", "verbatim")
+$propertyCount = $properties.Count
+
+Describe "$functionName mrdkwn Test Object Unit Tests" -Tags "Unit" {
+    It "An object is created" {
+        [bool](New-SlackTextObject -type "plain_text" -text $text) | Should -Be $true
+    }
+    It "has a type of plain_text" {
+        $mrkdwnObject.type | Should -BeExactly 'mrkdwn'
+    }
+    It "has text `"$text`"" {
+        $mrkdwnObject.text | Should -Be $text
+    }
+    It "has property count $propertyCount" {
+        $mrkdwnObject.PSObject.Properties.Name | Should -HaveCount $properties.Count
+    }
+    foreach ($property in $properties) {
+        It "has a $property property" {
+            [bool]($mrkdwnObject.PSObject.Properties.Name -match $property) | Should Be $true
         }
     }
-    Describe "Slack mrkdwn Object Acceptance tests" -Tags "Acceptance" {
-        $slackContent = Get-Content .\slacktoken.json | ConvertFrom-Json
-        $Body = $mrkdwnObject | ConvertTo-Json -Depth 100
-        It "returns http 200 response" {
-            $params = @{
-                Method      = "Post"
-                Uri         = $slackContent.slackwebhook
-                Headers     = @{Authorization = ("Bearer " + $slackContent.slacktoken) }
-                ContentType = "application/json"
-                Body        = $Body
-            }
-            Invoke-RestMethod @params | Should Be "ok"
+    It "is valid JSON" {
+        $mrkdwnObject | ConvertTo-Json -Depth 100
+    }
+}
+Describe "Slack mrkdwn Object Acceptance tests" -Tags "Acceptance" {
+    $slackContent = Get-Content .\slacktoken.json | ConvertFrom-Json
+    $Body = $mrkdwnObject | ConvertTo-Json -Depth 100
+    It "returns http 200 response" {
+        $params = @{
+            Method      = "Post"
+            Uri         = $slackContent.slackwebhook
+            Headers     = @{Authorization = ("Bearer " + $slackContent.slacktoken) }
+            ContentType = "application/json"
+            Body        = $Body
         }
+        Invoke-RestMethod @params | Should Be "ok"
     }
 }
