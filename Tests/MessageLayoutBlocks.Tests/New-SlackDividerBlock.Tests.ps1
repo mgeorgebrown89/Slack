9087030d054833dc02e40b7675dc2dc9c21e945e
Michael Brown <michael.brown@Viewpoint.com>
2019-06-06 09:37:45 -0700

    divider unit and acceptance tests

--- Tests/MessageLayoutBlocks.Tests/New-SlackDividerBlock.Tests.ps1
+++ Tests/MessageLayoutBlocks.Tests/New-SlackDividerBlock.Tests.ps1
@@ -1,52 +1,117 @@
-# If the module is already in memory, remove it
-Get-Module PSlickPSlack | Remove-Module -Force
+begin {
+    # If the module is already in memory, remove it
+    Get-Module PSlickPSlack | Remove-Module -Force
 
-# Import the module from the local path, not from the users Documents folder
-Import-Module .\PSlickPSlack\PSlickPSlack.psm1 -Force 
-$functionName = $MyInvocation.MyCommand -replace ".Tests.ps1",""
-Describe "$functionName Unit Tests" -Tags "Unit" {
-    Context "Slack Divider Block" {
-        $divider = New-SlackDividerBlock
-        $properties = @("type")
-        $propertyCount = $properties.Count
+    # Import the module from the local path, not from the users Documents folder
+    Import-Module .\PSlickPSlack\PSlickPSlack.psm1 -Force 
+    $functionName = $MyInvocation.MyCommand -replace ".Tests.ps1", ""
 
-        It "has a type of divider" {
-            $divider.type | Should Be 'divider'
-        }
-        It "has property count $propertyCount" {
-            $divider.PSObject.Properties.Name | Should -HaveCount $properties.Count
-        }
-        foreach ($property in $properties) {
-            It "has a $property property" {
-                [bool]($divider.PSObject.Properties.Name -match $property) | Should Be $true
+    if ($env:APPVEYOR) {
+        $SlackUri = $env:slackwebhook
+        $SlackHeaders = @{Authorization = ("Bearer " + $ev:slacktoken) }
+    }
+    else {
+        $slackContent = Get-Content .\slacktoken.json | ConvertFrom-Json
+        $SlackUri = $slackContent.slackwebhook
+        $SlackHeaders = @{Authorization = ("Bearer " + $slackContent.slacktoken) }
+    }
+
+    $divider = New-SlackDividerBlock
+
+    $block_id = "dividerblock123"
+    $dividerWithBlockId = New-SlackDividerBlock -block_id $block_id
+}
+process{
+    Describe "$functionName | Unit Tests" -Tags "Unit" {
+
+        Context "Slack Divider Block | Unit Tests" {
+    
+            $properties = @("type")
+            $propertyCount = $properties.Count
+    
+            It "has a type of divider" {
+                $divider.type | Should Be 'divider'
+            }
+            It "has property count $propertyCount" {
+                $divider.PSObject.Properties.Name | Should -HaveCount $properties.Count
+            }
+            foreach ($property in $properties) {
+                It "has a $property property" {
+                    [bool]($divider.PSObject.Properties.Name -match $property) | Should Be $true
+                }
+            }
+            It "is valid JSON" {
+                $divider | ConvertTo-Json -Depth 100
             }
         }
-        It "is valid JSON" {
-            $divider | ConvertTo-Json -Depth 100
+    
+        Context "Slack Divider Block with block_id | Unit Tests" {
+    
+            $properties = @("type","block_id")
+            $propertyCount = $properties.Count
+    
+            It "has a type of divider" {
+                $dividerWithBlockId.type | Should Be 'divider'
+            }
+            It "has a block_id of $block_id" {
+                $dividerWithBlockId.block_id | Should Be $block_id
+            }
+            It "has property count $propertyCount" {
+                $dividerWithBlockId.PSObject.Properties.Name | Should -HaveCount $properties.Count
+            }
+            foreach ($property in $properties) {
+                It "has a $property property" {
+                    [bool]($dividerWithBlockId.PSObject.Properties.Name -match $property) | Should Be $true
+                }
+            }
+            It "is valid JSON" {
+                $dividerWithBlockId | ConvertTo-Json -Depth 100
+            }
         }
     }
-    Context "Slack Divider Block with block_id" {
-        $block_id = "blockABC123"
-        $divider = New-SlackDividerBlock -block_id $block_id
-        $properties = @("type","block_id")
-        $propertyCount = $properties.Count
-
-        It "has a type of divider" {
-            $divider.type | Should Be 'divider'
-        }
-        It "has a block_id of $block_id" {
-            $divider.block_id | Should Be $block_id
-        }
-        It "has property count $propertyCount" {
-            $divider.PSObject.Properties.Name | Should -HaveCount $properties.Count
-        }
-        foreach ($property in $properties) {
-            It "has a $property property" {
-                [bool]($divider.PSObject.Properties.Name -match $property) | Should Be $true
+    
+    Describe "$functionName | Acceptance Tests" -Tags "Acceptance" {
+    
+        Context "Slack Divider Block | Acceptance Tests" {
+    
+            $Blocks = @()
+            $Blocks += $divider
+            $Body = @{
+                blocks = $Blocks
+            }
+            $params = @{
+                Method      = "Post"
+                Uri         = $SlackUri
+                Headers     = $SlackHeaders
+                ContentType = "application/json"
+                Body        = $Body | ConvertTo-Json -Depth 100
+            }
+    
+            It "returns http 200 response" {
+                $response = Invoke-RestMethod @params 
+                $response | Should Be "ok"
             }
         }
-        It "is valid JSON" {
-            $divider | ConvertTo-Json -Depth 100
+    
+        Context "Slack Divider Block with block_id | Acceptance Tests" {
+    
+            $Blocks = @()
+            $Blocks += $dividerWithBlockId
+            $Body = @{
+                blocks = $Blocks
+            }
+            $params = @{
+                Method      = "Post"
+                Uri         = $SlackUri
+                Headers     = $SlackHeaders
+                ContentType = "application/json"
+                Body        = $Body | ConvertTo-Json -Depth 100
+            }
+    
+            It "returns http 200 response" {
+                $response = Invoke-RestMethod @params 
+                $response | Should Be "ok"
+            }
         }
     }
 }
