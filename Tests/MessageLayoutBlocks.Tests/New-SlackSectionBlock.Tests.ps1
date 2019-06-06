2f3acae4350f75cdcb284b43894342a2854f93e3
Michael Brown <michael.brown@Viewpoint.com>
2019-06-06 10:00:20 -0700

    section block tests

--- Tests/MessageLayoutBlocks.Tests/New-SlackSectionBlock.Tests.ps1
+++ Tests/MessageLayoutBlocks.Tests/New-SlackSectionBlock.Tests.ps1
@@ -1,233 +1,234 @@
-# If the module is already in memory, remove it
-Get-Module PSlickPSlack | Remove-Module -Force
+begin {
+    # If the module is already in memory, remove it
+    Get-Module PSlickPSlack | Remove-Module -Force
 
-# Import the module from the local path, not from the users Documents folder
-Import-Module .\PSlickPSlack\PSlickPSlack.psm1 -Force 
-$functionName = $MyInvocation.MyCommand -replace ".Tests.ps1", ""
+    # Import the module from the local path, not from the users Documents folder
+    Import-Module .\PSlickPSlack\PSlickPSlack.psm1 -Force 
+    $functionName = $MyInvocation.MyCommand -replace ".Tests.ps1", ""
 
-if ($env:APPVEYOR) {
-    $SlackUri = $env:slackwebhook
-    $SlackHeaders = @{Authorization = ("Bearer " + $ev:slacktoken) }
+    if ($env:APPVEYOR) {
+        $SlackUri = $env:slackwebhook
+        $SlackHeaders = @{Authorization = ("Bearer " + $ev:slacktoken) }
+    }
+    else {
+        $slackContent = Get-Content .\slacktoken.json | ConvertFrom-Json
+        $SlackUri = $slackContent.slackwebhook
+        $SlackHeaders = @{Authorization = ("Bearer " + $slackContent.slacktoken) }
+    }
+
+    #section block
+    $text = "This is a Slack Section Block with just text."
+    $block = New-SlackSectionBlock -text $text
+
+    #section block with text and block_id
+    $text1 = "This is a Slack Section Block with text and a block_id."
+    $block_id = "blockABC123"
+    $blockWithTextAndBlockId = New-SlackSectionBlock -text $text1 -block_id $block_id
+
+    #section block with text and fields
+    $text2 = "This is a Slack Section Black with text and fields."
+    $fields = @()
+    $field1 = New-SlackTextObject -type mrkdwn -text "*Field 1 Title*`nField 1 Text"
+    $fields += $field1
+    $field2 = New-SlackTextObject -type plain_text -text "*Field 2 Title*`nField 2 Text"
+    $fields += $field2
+    $fieldCount = $fields.Count
+    $blockWithTextAndFields = New-SlackSectionBlock -text $text2 -fields $fields
+
+    #section block with text and an accessory
+    $accessory = New-SlackButtonElement -text "ButtonText" -action_id "actionid_123"
+    $text3 = "This is a Slack Section Black with text and an accessory."
+    $blockWithTextAndAccessory = New-SlackSectionBlock -text $text3 -accessory $accessory
 }
-else {
-    $slackContent = Get-Content .\slacktoken.json | ConvertFrom-Json
-    $SlackUri = $slackContent.slackwebhook
-    $SlackHeaders = @{Authorization = ("Bearer " + $slackContent.slacktoken) }
-}
+process {
+    Describe "$functionName | Unit Tests" -Tags "Unit" {
 
-Describe "$functionName | Unit Tests" -Tags "Unit" {
-
-    Context "Slack Section Block with text | Unit Tests" {
-
-        $text = "This is a Slack Section Block with just text."
-        $block = New-SlackSectionBlock -text $text
-        $properties = ("type", "text")
-        $propertyCount = $properties.Count
-
-        It "has a type of section" {
-            $block.type | Should Be 'section'
-        }
-        It "has text `"$text`"" {
-            $block.text.text | Should Be $text
-        }
-        It "has property count $propertyCount" {
-            $block.PSObject.Properties.Name | Should -HaveCount $properties.Count
-        }
-        foreach ($property in $properties) {
-            It "has a $property property" {
-                [bool]($block.PSObject.Properties.Name -match $property) | Should Be $true
+        Context "Slack Section Block with text | Unit Tests" {
+    
+            $properties = ("type", "text")
+            $propertyCount = $properties.Count
+    
+            It "has a type of section" {
+                $block.type | Should Be 'section'
+            }
+            It "has text `"$text`"" {
+                $block.text.text | Should Be $text
+            }
+            It "has property count $propertyCount" {
+                $block.PSObject.Properties.Name | Should -HaveCount $properties.Count
+            }
+            foreach ($property in $properties) {
+                It "has a $property property" {
+                    [bool]($block.PSObject.Properties.Name -match $property) | Should Be $true
+                }
+            }
+            It "is valid JSON" {
+                $block | ConvertTo-Json -Depth 100
             }
         }
-        It "is valid JSON" {
-            $block | ConvertTo-Json -Depth 100
-        }
-    }
-
-    Context "Slack Section Block with text and block_id | Unit Tests" {
-        
-        $text = "This is a Slack Section Block with text and a block_id."
-        $block_id = "blockABC123"
-        $block = New-SlackSectionBlock -text $text -block_id $block_id
-        $properties = ("type", "text", "block_id")
-        $propertyCount = $properties.Count
-
-        It "has a type of section" {
-            $block.type | Should Be "section"
-        }
-        It "has text `"$text`"" {
-            $block.text.text | Should Be $text
-        }
-        It "has block_id `"$block_id`"" {
-            $block.block_id | Should Be $block_id
-        }
-        It "has property count $propertyCount" {
-            $block.PSObject.Properties.Name | Should -HaveCount $properties.Count
-        }
-        foreach ($property in $properties) {
-            It "has a $property property" {
-                [bool]($block.PSObject.Properties.Name -match $property) | Should Be $true
+    
+        Context "Slack Section Block with text and block_id | Unit Tests" {
+            
+            $properties = ("type", "text", "block_id")
+            $propertyCount = $properties.Count
+    
+            It "has a type of section" {
+                $blockWithTextAndBlockId.type | Should Be "section"
+            }
+            It "has text `"$text1`"" {
+                $blockWithTextAndBlockId.text.text | Should Be $text1
+            }
+            It "has block_id `"$block_id`"" {
+                $blockWithTextAndBlockId.block_id | Should Be $block_id
+            }
+            It "has property count $propertyCount" {
+                $blockWithTextAndBlockId.PSObject.Properties.Name | Should -HaveCount $properties.Count
+            }
+            foreach ($property in $properties) {
+                It "has a $property property" {
+                    [bool]($blockWithTextAndBlockId.PSObject.Properties.Name -match $property) | Should Be $true
+                }
+            }
+            It "is valid JSON" {
+                $blockWithTextAndBlockId | ConvertTo-Json -Depth 100
             }
         }
-        It "is valid JSON" {
-            $block | ConvertTo-Json -Depth 100
-        }
-    }
-
-    Context "Slack Section Block with text and fields | Unit Tests" {
-
-        $text = "This is a Slack Section Black with text and fields."
-        $fields = @()
-        $field1 = New-SlackTextObject -type mrkdwn -text "*Field 1 Title*`nField 1 Text"
-        $fields += $field1
-        $field2 = New-SlackTextObject -type plain_text -text "*Field 2 Title*`nField 2 Text"
-        $fields += $field2
-        $fieldCount = $fields.Count
-        $block = New-SlackSectionBlock -text $text -fields $fields
-        $properties = ("type", "text", "fields")
-        $propertyCount = $properties.Count
-
-        It "has a type of section" {
-            $block.type | Should Be "section"
-        }
-        It "has text `"$text`"" {
-            $block.text.text | Should Be $text
-        }
-        It "has property count $propertyCount" {
-            $block.PSObject.Properties.Name | Should -HaveCount $properties.Count
-        }
-        foreach ($property in $properties) {
-            It "has a $property property" {
-                [bool]($block.PSObject.Properties.Name -match $property) | Should Be $true
+    
+        Context "Slack Section Block with text and fields | Unit Tests" {
+    
+            $properties = ("type", "text", "fields")
+            $propertyCount = $properties.Count
+    
+            It "has a type of section" {
+                $blockWithTextAndFields.type | Should Be "section"
+            }
+            It "has text `"$text2`"" {
+                $blockWithTextAndFields.text.text | Should Be $text2
+            }
+            It "has property count $propertyCount" {
+                $blockWithTextAndFields.PSObject.Properties.Name | Should -HaveCount $properties.Count
+            }
+            foreach ($property in $properties) {
+                It "has a $property property" {
+                    [bool]($blockWithTextAndFields.PSObject.Properties.Name -match $property) | Should Be $true
+                }
+            }
+            It "has field count $fieldCount" {
+                $blockWithTextAndFields.fields | Should -HaveCount $fields.Length
+            }
+            It "is valid JSON" {
+                $blockWithTextAndFields | ConvertTo-Json -Depth 100
             }
         }
-        It "has field count $fieldCount" {
-            $block.fields | Should -HaveCount $fields.Length
-        }
-        It "is valid JSON" {
-            $block | ConvertTo-Json -Depth 100
-        }
-    }
-
-    Context "Slack Section Block with text and accessory" {
-
-        $accessory = New-SlackButtonElement -text "ButtonText" -action_id "actionid_123"
-        $text = "This is a Slack Section Black with text and an accessory."
-        $block = New-SlackSectionBlock -text $text -accessory $accessory
-        $properties = ("type", "text", "accessory")
-        $propertyCount = $properties.Count
-
-        It "has a type of section" {
-            $block.type | Should Be "section"
-        }
-        It "has text `"$text`"" {
-            $block.text.text | Should Be $text
-        }
-        It "has property count $propertyCount" {
-            $block.PSObject.Properties.Name | Should -HaveCount $properties.Count
-        }
-        foreach ($property in $properties) {
-            It "has a $property property" {
-                [bool]($block.PSObject.Properties.Name -match $property) | Should Be $true
+    
+        Context "Slack Section Block with text and accessory" {
+    
+            $properties = ("type", "text", "accessory")
+            $propertyCount = $properties.Count
+    
+            It "has a type of section" {
+                $blockWithTextAndFields.type | Should Be "section"
+            }
+            It "has text `"$text3`"" {
+                $blockWithTextAndAccessory.text.text | Should Be $text3
+            }
+            It "has property count $propertyCount" {
+                $blockWithTextAndAccessory.PSObject.Properties.Name | Should -HaveCount $properties.Count
+            }
+            foreach ($property in $properties) {
+                It "has a $property property" {
+                    [bool]($blockWithTextAndAccessory.PSObject.Properties.Name -match $property) | Should Be $true
+                }
+            }
+            It "is valid JSON" {
+                $blockWithTextAndAccessory | ConvertTo-Json -Depth 100
             }
         }
-        It "is valid JSON" {
-            $block | ConvertTo-Json -Depth 100
-        }
     }
-}
-Describe "$functionName | Acceptance Tests" -Tags "Acceptance" {
+    Describe "$functionName | Acceptance Tests" -Tags "Acceptance" {
+    
+        Context "Text only Slack Section Block | Acceptance Tests" {
 
-    Context "Text only Slack Section Block | Acceptance Tests" {
-
-        $text = "This is a Slack Section Block with just text."
-        $block = @()
-        $block += New-SlackSectionBlock -text $text
-        $Body = @{
-            blocks = $block
+            $blocks = @()
+            $blocks += $block
+            $Body = @{
+                blocks = $blocks
+            }
+            $params = @{
+                Method      = "Post"
+                Uri         = $SlackUri
+                Headers     = $SlackHeaders
+                ContentType = "application/json"
+                Body        = $Body | ConvertTo-Json -Depth 100
+            }
+    
+            It "returns http 200 response" {
+                $response = Invoke-RestMethod @params 
+                $response | Should Be "ok"
+            }
         }
-        $params = @{
-            Method      = "Post"
-            Uri         = $SlackUri
-            Headers     = $SlackHeaders
-            ContentType = "application/json"
-            Body        = $Body | ConvertTo-Json -Depth 100
+    
+        Context "Slack Section Block with text and block_id | Acceptance Tests" {
+    
+            $blocks = @()
+            $blocks += $blockWithTextAndBlockId
+            $Body = @{
+                blocks = $blocks
+            }
+            $params = @{
+                Method      = "Post"
+                Uri         = $SlackUri
+                Headers     = $SlackHeaders
+                ContentType = "application/json"
+                Body        = $Body | ConvertTo-Json -Depth 100
+            }
+    
+            It "returns http 200 response" {
+                $response = Invoke-RestMethod @params
+                $response | Should Be "ok"
+            }
         }
-
-        It "returns http 200 response" {
-            $response = Invoke-RestMethod @params 
-            $response | Should Be "ok"
+    
+        Context "Slack Section Block with text and fields | Acceptance Tests" {
+    
+            $blocks = @()
+            $blocks += $blockWithTextAndFields
+            $Body = @{
+                blocks = $blocks
+            }
+            $params = @{
+                Method      = "Post"
+                Uri         = $SlackUri
+                Headers     = $SlackHeaders
+                ContentType = "application/json"
+                Body        = $Body | ConvertTo-Json -Depth 100
+            }
+    
+            It "returns http 200 response" {
+                $response = Invoke-RestMethod @params
+                $response | Should Be "ok"
+            }
         }
+    
+        Context "Slack Section Block with text and accessory | Acceptance Tests" {
+            
+            $blocks = @()
+            $blocks += $blockWithTextAndAccessory
+            $Body = @{
+                blocks = $blocks
+            }
+            $params = @{
+                Method      = "Post"
+                Uri         = $SlackUri
+                Headers     = $SlackHeaders
+                ContentType = "application/json"
+                Body        = $Body | ConvertTo-Json -Depth 100
+            }
+    
+            It "returns http 200 response" {
+                $response = Invoke-RestMethod @params
+                $response | Should Be "ok"
+            }
+        } 
     }
-
-    Context "Slack Section Block with text and block_id | Acceptance Tests" {
-
-        $text = "This is a Slack Section Block with text and block_id."
-        $blocks = @()
-        $blocks += New-SlackSectionBlock -text $text -block_id "block456"
-        $Body = @{
-            blocks = $blocks
-        }
-        $params = @{
-            Method      = "Post"
-            Uri         = $SlackUri
-            Headers     = $SlackHeaders
-            ContentType = "application/json"
-            Body        = $Body | ConvertTo-Json -Depth 100
-        }
-
-        It "returns http 200 response" {
-            $response = Invoke-RestMethod @params
-            $response | Should Be "ok"
-        }
-    }
-
-    Context "Slack Section Block with text and fields | Acceptance Tests" {
-
-        $text = "This is a Slack Section Black with text and fields."
-        $fields = @()
-        $blocks = @()
-        $field1 = New-SlackTextObject -type mrkdwn -text "*Field 1 Title*`nField 1 Text"
-        $fields += $field1
-        $field2 = New-SlackTextObject -type plain_text -text "*Field 2 Title*`nField 2 Text"
-        $fields += $field2
-        $blocks += New-SlackSectionBlock -text $text -fields $fields
-        $Body = @{
-            blocks = $blocks
-        }
-        $params = @{
-            Method      = "Post"
-            Uri         = $SlackUri
-            Headers     = $SlackHeaders
-            ContentType = "application/json"
-            Body        = $Body | ConvertTo-Json -Depth 100
-        }
-
-        It "returns http 200 response" {
-            $response = Invoke-RestMethod @params
-            $response | Should Be "ok"
-        }
-    }
-
-    Context "Slack Section Block with text and accessory | Acceptance Tests" {
-        
-        $blocks = @()
-        $accessory = New-SlackButtonElement -text "ButtonText" -action_id "ButtonAction_id"
-        $text = "This is a Slack Section Block with an accessory."
-        $blocks += New-SlackSectionBlock -text $text -accessory $accessory
-        $Body = @{
-            blocks = $blocks
-        }
-        $params = @{
-            Method      = "Post"
-            Uri         = $SlackUri
-            Headers     = $SlackHeaders
-            ContentType = "application/json"
-            Body        = $Body | ConvertTo-Json -Depth 100
-        }
-
-        It "returns http 200 response" {
-            $response = Invoke-RestMethod @params
-            $response | Should Be "ok"
-        }
-    } 
 }
