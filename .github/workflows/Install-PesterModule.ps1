96346846d6b709e281b5a5c8636a841b2cc71c9f
Michael Brown <michael.brown@Viewpoint.com>
2019-09-11 14:53:07 -0700

    forgot to uncomment out pester stuff

--- .github/workflows/Install-PesterModule.ps1
+++ .github/workflows/Install-PesterModule.ps1
@@ -1,8 +1,8 @@
-<#Write-Host "Installing Pester from PSGallery"
+Write-Host "Installing Pester from PSGallery"
 try {
     Install-PackageProvider -Name NuGet -Force -Scope CurrentUser -ErrorAction SilentlyContinue
 }
 catch {
     Write-Host "NuGet not needed."
 } 
-Install-Module -Name Pester -Force -Scope CurrentUser#>
+Install-Module -Name Pester -Force -Scope CurrentUser
