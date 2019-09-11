ec9710d5dbcfe0babc13d1bd8ab109d60ff9e278
Michael Brown <michael.brown@Viewpoint.com>
2019-09-11 14:48:52 -0700

    release notes worked. let's release!

--- .github/workflows/Deploy-PSlickPSlack.ps1
+++ .github/workflows/Deploy-PSlickPSlack.ps1
@@ -1,4 +1,4 @@
 $tags = @("PowerShell", "Slack", "API")
 $Path = (Split-Path -Parent (Split-Path -Parent (Split-Path -Parent $MyInvocation.MyCommand.Path))) + "\PSlickPSlack"
-$env:RELEASE_NOTES
-#Publish-Module -Path $Path -NuGetApiKey $env:PSGALLERY_TOKEN -Tags $tags -Force
+$releaseNotes = $env:RELEASE_NOTES
+Publish-Module -Path $Path -NuGetApiKey $env:PSGALLERY_TOKEN -Tags $tags -ReleaseNotes $releaseNotes -Force
