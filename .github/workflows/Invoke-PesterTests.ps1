2ecb00fd4f8d9c3029b0bf52b0f7ab21435f0497
Michael Brown <michael.brown@viewpoint.com>
2019-09-10 19:44:01 -0700

    testing secrets

--- .github/workflows/Invoke-PesterTests.ps1
+++ .github/workflows/Invoke-PesterTests.ps1
@@ -1,6 +1,8 @@
 $root = Split-Path -Parent $MyInvocation.MyCommand.Path
 
-$result = Invoke-Pester -OutputFile "$root\PSlickPSlack.Tests.xml" -OutputFormat NUnitXML -PassThru
+Write-Host "Slack Channel Test" $env:SLACK_CHANNEL
+
+#$result = Invoke-Pester -OutputFile "$root\PSlickPSlack.Tests.xml" -OutputFormat NUnitXML -PassThru
 
 if ($result.failedCount -ne 0) { 
     $x = $result.failedCount
