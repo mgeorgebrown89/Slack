f47a01f9d41517af5a400a9079f5bba1b4ea59b6
Michael Brown <michael.brown@viewpoint.com>
2019-09-10 19:47:33 -0700

    let's see output baby

--- .github/workflows/Invoke-PesterTests.ps1
+++ .github/workflows/Invoke-PesterTests.ps1
@@ -2,6 +2,20 @@ $root = Split-Path -Parent $MyInvocation.MyCommand.Path
 
 Write-Host "Slack Channel Test" $env:SLACK_CHANNEL
 
+$token = $env:SLACK_TOKEN
+$channel = $env:SLACK_CHANNEL
+
+$Headers = @{
+Authorization = "Bearer $token"
+}
+
+$Body = [PSCustomObject]@{
+    channel = $channel
+    text    = "Hello, from GitHub Actions!"
+}
+
+Invoke-RestMethod -Method Post -Uri 'https://slack.com/api/chat.postMessage' -Headers $Headers -ContentType 'application/json;charset=iso-8859-1' -Body ($Body | ConvertTo-Json -Depth 100)
+
 #$result = Invoke-Pester -OutputFile "$root\PSlickPSlack.Tests.xml" -OutputFormat NUnitXML -PassThru
 
 if ($result.failedCount -ne 0) { 
