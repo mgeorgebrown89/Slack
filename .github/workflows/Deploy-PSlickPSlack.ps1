87291d4ded41cf027038c74c676e476686e3343f
Michael Brown <michael.brown@Viewpoint.com>
2019-09-12 14:07:08 -0700

    adding -debug

--- .github/workflows/Deploy-PSlickPSlack.ps1
+++ .github/workflows/Deploy-PSlickPSlack.ps1
@@ -4,12 +4,13 @@ $releaseNotes = $env:RELEASE_NOTES
 $moduleVersion = ($env:RELEASE_VERSION) -replace "v",""
 Write-Host "ModuleVersion: $moduleVersion"
 $manifestPath = Resolve-Path -Path "*\PSlickPSlack.psd1" -Verbose
-Update-ModuleManifest -ReleaseNotes $releaseNotes -Path $manifestPath.Path -ModuleVersion $moduleVersion -Verbose
+#Update-ModuleManifest -ReleaseNotes $releaseNotes -Path $manifestPath.Path -ModuleVersion $moduleVersion -Verbose
 $moduleFilePath = Resolve-Path -Path "*\PSlickPSlack.psm1" -Verbose
 $modulePath = Split-Path -Parent $moduleFilePath.Path -Verbose
 $nuGetApiKey = $env:PSGALLERY_TOKEN
+$nuGetApiKey = "a"
 try{
-    Publish-Module -Path $modulePath -NuGetApiKey $nuGetApiKey -Repository PSGallery -Force -Verbose -ErrorAction Stop 
+    Publish-Module -Path $modulePath -NuGetApiKey $nuGetApiKey -Repository PSGallery -Force -Verbose -Debug -ErrorAction Stop 
     Write-Host "PSlickPSlack Module Published!"
 }
 catch {
