966a93695724b7416cf6629373e73395a6e9a38b
Michael Brown <michael.brown@Viewpoint.com>
2019-09-12 09:40:22 -0700

    resolve-path ftw!

--- .github/workflows/Deploy-PSlickPSlack.ps1
+++ .github/workflows/Deploy-PSlickPSlack.ps1
@@ -3,5 +3,5 @@ $moduleVersion = ($env:RELEASE_VERSION) -replace "v",""
 Write-Host "ModuleVersion: $moduleVersion"
 $manifestPath = Resolve-Path -Path "*\PSlickPSlack.psd1"
 Update-ModuleManifest -ReleaseNotes $releaseNotes -Path $manifestPath -ModuleVersion $moduleVersion
-
-Publish-Module -Path $Path -NuGetApiKey $env:PSGALLERY_TOKEN -Force
+$modulePath = Resolve-Path -Path "*\PSlickPSlack.psm1"
+Publish-Module -Path $modulePath -NuGetApiKey $env:PSGALLERY_TOKEN -Force
