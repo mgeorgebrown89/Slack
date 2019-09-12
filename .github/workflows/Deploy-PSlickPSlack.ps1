dab3f19757bf768d9e6a926bf5bae87e76632485
Michael Brown <michael.brown@Viewpoint.com>
2019-09-12 09:51:32 -0700

    ............paths..........

--- .github/workflows/Deploy-PSlickPSlack.ps1
+++ .github/workflows/Deploy-PSlickPSlack.ps1
@@ -1,10 +1,9 @@
-
 $releaseNotes = $env:RELEASE_NOTES
 $moduleVersion = ($env:RELEASE_VERSION) -replace "v",""
 $moduleVersion = "0.0.8"
 Write-Host "ModuleVersion: $moduleVersion"
 $manifestPath = Resolve-Path -Path "*\PSlickPSlack.psd1"
 Update-ModuleManifest -ReleaseNotes $releaseNotes -Path $manifestPath -ModuleVersion $moduleVersion
-$Path = (Split-Path -Parent (Split-Path -Parent (Split-Path -Parent $MyInvocation.MyCommand.Path)))
-Write-Host "Path: $Path"
-Publish-Module -Path $Path -NuGetApiKey $env:PSGALLERY_TOKEN -Force
+$moduleFilePath = Resolve-Path -Path "*\PSlickPSlack.psm1"
+$modulePath = Split-Path -Parent $moduleFilePath
+Publish-Module -Path $modulePath -NuGetApiKey $env:PSGALLERY_TOKEN -Force
