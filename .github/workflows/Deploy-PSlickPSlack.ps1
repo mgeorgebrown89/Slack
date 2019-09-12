db5507f87bcb74c5bba45218f67c43af7f431694
Michael Brown <michael.brown@Viewpoint.com>
2019-09-12 09:55:37 -0700

    removed explicit module version

--- .github/workflows/Deploy-PSlickPSlack.ps1
+++ .github/workflows/Deploy-PSlickPSlack.ps1
@@ -1,6 +1,6 @@
 $releaseNotes = $env:RELEASE_NOTES
 $moduleVersion = ($env:RELEASE_VERSION) -replace "v",""
-$moduleVersion = "0.0.8"
+
 Write-Host "ModuleVersion: $moduleVersion"
 $manifestPath = Resolve-Path -Path "*\PSlickPSlack.psd1"
 Update-ModuleManifest -ReleaseNotes $releaseNotes -Path $manifestPath -ModuleVersion $moduleVersion
