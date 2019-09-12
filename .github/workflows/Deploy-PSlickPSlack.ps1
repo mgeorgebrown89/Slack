e023886744c3b043f6e2c86f16f9d423b33fbd98
Michael Brown <michael.brown@Viewpoint.com>
2019-09-12 11:02:22 -0700

    please

--- .github/workflows/Deploy-PSlickPSlack.ps1
+++ .github/workflows/Deploy-PSlickPSlack.ps1
@@ -6,4 +6,4 @@ $manifestPath = Resolve-Path -Path "*\PSlickPSlack.psd1" -Verbose
 Update-ModuleManifest -ReleaseNotes $releaseNotes -Path $manifestPath -ModuleVersion $moduleVersion -Verbose
 $moduleFilePath = Resolve-Path -Path "*\PSlickPSlack.psm1" -Verbose
 $modulePath = Split-Path -Parent $moduleFilePath -Verbose
-Publish-Module -Path $modulePath -NuGetApiKey $env:PSGALLERY_TOKEN -Force -Verbose
+Publish-Module -Path $modulePath -NuGetApiKey $env:PSGALLERY_TOKEN -Repository PSGallery -Verbose
