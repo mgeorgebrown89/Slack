21019dfb67280a5678145613b85173e4332b0d11
Michael Brown <michael.brown@Viewpoint.com>
2019-09-17 08:45:24 -0700

    removing debug output

--- .github/workflows/Deploy-PSlickPSlack.ps1
+++ .github/workflows/Deploy-PSlickPSlack.ps1
@@ -5,21 +5,21 @@ $moduleVersion = ($env:RELEASE_VERSION) -replace "v",""
 Write-Host "ModuleVersion: $moduleVersion"
 
 $manifestPath = Resolve-Path -Path "*\PSlickPSlack.psd1"
-Write-Host "Manifest Path: $manifestPath"
+#Write-Host "Manifest Path: $manifestPath"
 
-Update-ModuleManifest -ReleaseNotes $releaseNotes -Path $manifestPath.Path -ModuleVersion $moduleVersion -Verbose
+Update-ModuleManifest -ReleaseNotes $releaseNotes -Path $manifestPath.Path -ModuleVersion $moduleVersion #-Verbose
 
 $moduleFilePath = Resolve-Path -Path "*\PSlickPSlack.psm1"
-Write-Host "Module File Path: $moduleFilePath"
+#Write-Host "Module File Path: $moduleFilePath"
 
 $modulePath = Split-Path -Parent $moduleFilePath
-Write-Host "Module Path: $modulePath"
+#Write-Host "Module Path: $modulePath"
 
 $nuGetApiKey = $env:PSGALLERY_TOKEN
 
 try{
-    Publish-Module -Path $modulePath -NuGetApiKey $nuGetApiKey -Debug -ErrorAction Stop -Force
-    Write-Host "PSlickPSlack Module Published!"
+    Publish-Module -Path $modulePath -NuGetApiKey $nuGetApiKey -ErrorAction Stop -Force #-Debug
+    Write-Host "The PSlickPSlack PowerShell Module Version $moduleVersion has been Published to the PowerShell Gallery!"
 }
 catch {
     throw $_
