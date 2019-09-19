39d98f1cfc91491ccef74333edd93eac7fc5a53d
Michael Brown <michael.brown@viewpoint.com>
2019-09-19 12:18:23 -0700

    hmmm

--- .github/workflows/Invoke-PesterTests.ps1
+++ .github/workflows/Invoke-PesterTests.ps1
@@ -1,6 +1,6 @@
 $root = Split-Path -Parent $MyInvocation.MyCommand.Path
 
-$result = Invoke-Pester -OutputFile "$root\PSlickPSlack.Tests.xml" -OutputFormat NUnitXML -PassThru
+$result = Invoke-Pester -OutputFile "$root\PSlickPSlack.Tests.xml" -OutputFormat NUnitXML -PassThru -Show Fails
 Write-Host "Root path: $root"
 if ($result.failedCount -ne 0) { 
     $x = $result.failedCount
--- .github/workflows/Publish-PesterTests.ps1
+++ .github/workflows/Publish-PesterTests.ps1
@@ -2,7 +2,7 @@ $root = Split-Path -Parent $MyInvocation.MyCommand.Path
 $root
 
 $testsFolderPath = Split-Path -Parent (Split-Path -Parent $root)
-$testsFolderPath = $testsFolderPath + "\Tests"
+$testsFolderPath = $testsFolderPath + "/Tests"
 $testsFolderPath
 
-& $testsFolderPath\ReportUnit.exe $root $root
+& $testsFolderPath/ReportUnit.exe $root $root
