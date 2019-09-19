69e56f3e978e076a1f84953263874b7330a180fc
Michael Brown <michael.brown@viewpoint.com>
2019-09-19 14:46:00 -0700

    testing github api

--- .github/workflows/Publish-PesterTests.ps1
+++ .github/workflows/Publish-PesterTests.ps1
@@ -3,3 +3,5 @@ $root = Split-Path -Parent $MyInvocation.MyCommand.Path
 $testResults = Get-Content -Path "$root\PSlickPSlack.Tests.json" | ConvertFrom-Json
 
 $testResults
+
+Invoke-RestMethod -Uri "https://api.github.com/repos/mgeorgebrown89/pslickpslack/pulls" -Method Get
