7c6db9b2e209c815f80adb9ea61c836d5a26d902
Michael Brown <michael.brown@Viewpoint.com>
2019-09-19 17:44:00 -0700

    setting up markdown

--- .github/workflows/Publish-PesterTests.ps1
+++ .github/workflows/Publish-PesterTests.ps1
@@ -4,11 +4,14 @@ $testResults = Get-Content -Path "$root\PSlickPSlack.Tests.json" | ConvertFrom-J
 
 $testResults
 
+$markdown = @"
+
+"@
+
 $Body = @{
-    body = "Will **markdown** be _formatted_?
-    
-    What about spaces and newlines?"
+    body = $markdown
 }
+
 $Token = $env:GITHUB_TOKEN
 Write-Host "Token: $Token"
 $Uri = $env:GITHUB_PR_URI
