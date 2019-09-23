0cac3cbdcc0d958e3c32e71bc8b7c3c2001277e2
Michael Brown <michael.brown@viewpoint.com>
2019-09-22 19:33:29 -0700

    q

--- .github/workflows/Publish-PesterTests.ps1
+++ .github/workflows/Publish-PesterTests.ps1
@@ -2,14 +2,65 @@ $root = Split-Path -Parent $MyInvocation.MyCommand.Path
 
 $testResults = Get-Content -Path "$root\PSlickPSlack.Tests.json" | ConvertFrom-Json
 #$testResults
+$total = $testResults.TotalCount
+$passed = $testResults.PassedCount
+$failed = $testResults.FailedCount
+$skipped = $testResults.SkippedCount
+$pending = $testResults.PendingCount
+$inconclusive = $testResults.InconclusiveCount
+$markdown = @"
+| Total | Passed | Failed | Skipped | Pending | Inconclusive |
+| --: | --: | --: | --: | --: | --: |
+| $total | $passed | $failed | $skipped | $pending | $inconclusive |
 
-$DescribeGroups = $testResults.TestResult | Group-Object -Property Describe
+<details>
+<summary><b>Failures</b></summary>
+<br>
+<table>
+  <tr>
+    <th colspan="3">Context</th>
+  </tr>
+  <tr>
+    <th>Passed</th>
+    <th>Name</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+  <tr>
+    <td>Ernst Handel</td>
+    <td>Roland Mendel</td>
+    <td>Austria</td>
+  </tr>
+  <tr>
+    <td>Island Trading</td>
+    <td>Helen Bennett</td>
+    <td>UK</td>
+  </tr>
+  <tr>
+    <td>Laughing Bacchus Winecellars</td>
+    <td>Yoshi Tannamuri</td>
+    <td>Canada</td>
+  </tr>
+  <tr>
+    <td>Magazzini Alimentari Riuniti</td>
+    <td>Giovanni Rovelli</td>
+    <td>Italy</td>
+  </tr>
+</table>
 
-$markdown = @'
-Passed | Name
---- | ---
+</details>
 
-'@
+"@
+
 foreach ($tr in $testResults.TestResult){
     if($tr.passed -eq 'True'){
         $passed = ":heavy_check_mark:"
