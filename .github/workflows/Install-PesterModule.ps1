dffa10093230e49dca5bc9c98db3e12c2fe7fed7
Michael Brown <michael.brown@viewpoint.com>
2019-09-28 09:33:53 -0700

    fixed test failure. trying different pester install instead of just forcing it

--- .github/workflows/Install-PesterModule.ps1
+++ .github/workflows/Install-PesterModule.ps1
@@ -5,4 +5,31 @@ try {
 catch {
     Write-Host "NuGet not needed."
 } 
-Install-Module -Name Pester -Force -Scope CurrentUser
+
+$ModuleName = 'Pester'
+$Module = Get-Module -Name "$ModuleName" -ListAvailable
+Write-Verbose -Message "Resolving Module $($ModuleName)"
+
+if ($Module) {                
+    $Version = $Module | Measure-Object -Property Version -Maximum | Select-Object -ExpandProperty Maximum
+    $GalleryVersion = Find-Module -Name $ModuleName -Repository PSGallery | Measure-Object -Property Version -Maximum | Select-Object -ExpandProperty Maximum
+
+    if ($Version -lt $GalleryVersion) {
+        
+        if ((Get-PSRepository -Name PSGallery).InstallationPolicy -ne 'Trusted') { Set-PSRepository -Name PSGallery -InstallationPolicy Trusted }
+        
+        Write-Verbose -Message "$($ModuleName) Installed Version [$($Version.tostring())] is outdated. Installing Gallery Version [$($GalleryVersion.tostring())]"
+        
+        Install-Module -Name $ModuleName -Force
+        Import-Module -Name $ModuleName -Force -RequiredVersion $GalleryVersion
+    }
+    else {
+        Write-Verbose -Message "Module Installed, Importing $($ModuleName)"
+        Import-Module -Name $ModuleName -Force -RequiredVersion $Version
+    }
+}
+else {
+    Write-Verbose -Message "$($ModuleName) Missing, installing Module"
+    Install-Module -Name $ModuleName -Force
+    Import-Module -Name $ModuleName -Force -RequiredVersion $Version
+}
--- Tests/Slack.Blocks/Public/LayoutBlocks/New-SlackActionsBlock.Tests.ps1
+++ Tests/Slack.Blocks/Public/LayoutBlocks/New-SlackActionsBlock.Tests.ps1
@@ -35,7 +35,7 @@ InModuleScope -ModuleName Slack {
             $propertyCount = $properties.Count
     
             It "has a type of actions" {
-                $actionsBlock.type | Should Be 'actionss'
+                $actionsBlock.type | Should Be 'actions'
             }
             It "has property count $propertyCount" {
                 $actionsBlock.PSObject.Properties.Name | Should -HaveCount $properties.Count
