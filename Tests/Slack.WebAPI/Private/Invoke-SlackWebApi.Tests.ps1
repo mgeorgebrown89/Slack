b7ebb4abbfe499210ae2a78332102296cf430c1e
Michael Brown <MBrown2@am.trimblecorp.net>
2019-10-31 10:52:13 -0700

    missed a bad path

--- Tests/Slack.WebAPI/Private/Invoke-SlackWebApi.Tests.ps1
+++ Tests/Slack.WebAPI/Private/Invoke-SlackWebApi.Tests.ps1
@@ -2,13 +2,9 @@
 Get-Module Slack | Remove-Module -Force
 
 # Import the module from the local path, not from the users Documents folder
-$repoRoot = Split-Path -Parent (Split-Path -Parent (Split-Path -Parent (Split-Path -Parent ((Split-Path -Parent $MyInvocation.MyCommand.Path)))))
+$repoRoot = Split-Path -Parent (Split-Path -Parent (Split-Path -Parent ((Split-Path -Parent $MyInvocation.MyCommand.Path))))
 $ModuleRoot = $repoRoot + "\Slack"
 Import-Module $ModuleRoot -Force 
-
-$slackContent = Get-Content $ModuleRoot\SlackDefaults.json | ConvertFrom-Json
-$SlackUri = $slackContent.slackwebhook
-$SlackHeaders = @{Authorization = ("Bearer " + $slackContent.slacktoken) }
 
 InModuleScope -ModuleName Slack {
     
